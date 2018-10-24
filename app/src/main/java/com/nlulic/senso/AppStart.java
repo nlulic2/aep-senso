e3df23255e84ef1ef87466e124b3a7abbc20cfd0
nlulic <nikola.lulic@ixts.de>
2018-10-24 20:40:24 +0200

    initial commit

---
+++ app/src/main/java/com/nlulic/senso/AppStart.java
@@ -0,0 +1,13 @@
+package com.nlulic.senso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_start);
+    }
+}
