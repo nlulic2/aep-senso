a165c2b935398fe1830f11681486dbea5026a52f
nlulic <nikola.lulic@ixts.de>
2018-10-25 22:10:32 +0200

    added bease concept of game api

--- app/src/main/java/com.nlulic.senso/Business/SensoGame.java
+++ app/src/main/java/com.nlulic.senso/Business/SensoGame.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
-
 public class SensoGame {
+
+    //pattern list
+    //current return index
+    //rounds
+
+    SensoGame() {
+        //maybe set type to single/multiplayer/online
+    }
+
+    public void Run() {
+        //initializes the pattern
+    }
+
+    public void Next() {
+        //gets next pattern
+    }
+
+    public void Assert() {
+        //Asserts user selection and pattern
+    }
+
+    public void Append() {
+        //Adds new value to the pattern
+    }
+
 }
